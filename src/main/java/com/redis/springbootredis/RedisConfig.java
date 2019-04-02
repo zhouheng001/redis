26789e60dd3733218391df7362107ad95ad37940
wb-zh388722 <wb-zh388722@alibaba-inc.com>
2019-04-02 20:15:27 +0800

    redis工具

---
+++ src/main/java/com/redis/springbootredis/RedisConfig.java
@@ -0,0 +1,46 @@
+package com.redis.springbootredis;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.time.Duration;
+
+/**
+ * redis配置类
+ *
+ * @author zcc ON 2018/3/19
+ **/
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1)); // 设置缓存有效期一小时
+        return RedisCacheManager
+                .builder(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory))
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        return stringRedisTemplate;
+    }
+}
--- src/test/java/com/redis/springbootredis/SpringbootredisApplicationTests.java
+++ src/test/java/com/redis/springbootredis/SpringbootredisApplicationTests.java
@@ -2,15 +2,41 @@ package com.redis.springbootredis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootredisApplicationTests {
 
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+
 	@Test
 	public void contextLoads() {
+//		stringRedisTemplate.opsForValue().set("zhou","jjsjf");
+//		Set<String> zhou = stringRedisTemplate.keys("zhou");
+//		stringRedisTemplate.opsForZSet().add("1","123",5);
+//		stringRedisTemplate.opsForZSet().add("1","123456",4);
+//		stringRedisTemplate.opsForZSet().add("1","1234",8);
+
+
+		Set<String> range = stringRedisTemplate.opsForZSet().range("1", 0, -1);
+		Set<String> strings = stringRedisTemplate.opsForZSet().rangeByScore("1", 4, 5);
+		Set<String> strings1 = stringRedisTemplate.opsForZSet().reverseRangeByScore("1",1,3);
+		for (String s : strings1) {
+			System.out.println(s);
+		}
+
 	}
 
 }
